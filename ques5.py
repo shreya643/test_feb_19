161003b7333aa3697120e78b65b54db30022ebb5
shreya643 <shreya998.ss@gmail.com>
2018-02-19 11:55:21 +0545

    ques5 completed

---
+++ ques5.py
@@ -0,0 +1,18 @@
+#5. Define an Student class and initialize it with name and section.
+# Now, make a classmethod that takes in a string parameter "name-A" which creates an instance and
+# returns the instance based on parameter.
+# [Hint: use @classmethod decorator]
+
+class Student:
+    def __init__(self,name,sem):
+        self.name=name
+        self.sem=sem
+
+    @classmethod
+    def get_from_string(cls,inp):
+        name,sem=inp.split("-")
+        return cls(name,sem)
+
+
+student1=Student.get_from_string("Shreya-A")
+print student1
