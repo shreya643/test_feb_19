5f7893b6c0ca94cc792fb7a290e267eb52bcb03c
shreya643 <shreya998.ss@gmail.com>
2018-02-19 11:44:50 +0545

    ques3 completed

---
+++ ques3.py
@@ -0,0 +1,19 @@
+#Q3. Create a Employee class and initialize it with first_name, last_name and salary. Also, it has a derived attribute called email, which is self generated when instance is created. Now, make methods to :
+#a. Display - It should display all information of the employee instance.
+
+class Employee:
+
+    def __init__(self,first_name,last_name,salary):
+        self.first_name=first_name
+        self.last_name=last_name
+        self.salary=salary
+
+    def display(self):
+        print ("First name: {}".format(self.first_name))
+        print ("Last Name:{}".format(self.last_name))
+        print("Salay:{}".format(self.salary))
+
+
+
+emp1=Employee('Shreya','Sapkota',800)
+emp1.display()
