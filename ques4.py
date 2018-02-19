7bdbfdc413e7b96a8a7ffaf92925eaecfe464a3f
shreya643 <shreya998.ss@gmail.com>
2018-02-19 12:13:41 +0545

    ques4 completed

--- ques4.py
+++ ques4.py
@@ -2,4 +2,16 @@
 # a. convertFahrenheit - It will take Celsius and will print it into Fahrenheit.
 # b. convertCelsius - It will take Fahrenheit and will convert it into Celsius.
 
-class Temper
+class Temperature:
+
+    def convertFahrenheit(c):
+        print ("Fahrenheit={}".format(1.8 * c + 32))
+
+
+    def convertCelsius(f):
+        print ("Celsius={}".format((f-32)/1.8))
+
+
+temp1= Temperature
+temp1.convertCelsius(10)
+temp1.convertFahrenheit(32)
