2b44c88cf2eafb0fee40a616ef5b24c20f2b1eca
shreya643 <shreya998.ss@gmail.com>
2018-02-19 12:22:37 +0545

    ques4 completed

--- ques4.py
+++ ques4.py
@@ -1,17 +1,20 @@
 '''Q4. Create a Temperature class. Make two methods :
 a. convertFahrenheit - It will take Celsius and will print it into Fahrenheit.
 b. convertCelsius - It will take Fahrenheit and will convert it into Celsius.'''
+
 class Temperature:
+    def __init__(self,c,f):
+        self.c=c
+        self.f=f
 
-    def convertFahrenheit(c):
-        print ("Fahrenheit={}".format(1.8 * c + 32))
-
-
-
-    def convertCelsius(f):
-        print ("Celsius={}".format((f-32)/1.8))
+    def convertFahrenheit(self):
+        print ("Fahrenheit={}".format(1.8 * self.c + 32))
 
 
-temp1= Temperature
-temp1.convertCelsius(10)
-temp1.convertFahrenheit(32)
+    def convertCelsius(self):
+        print ("Celsius={}".format((self.f-32)/1.8))
+
+
+temp1= Temperature(10,32)
+temp1.convertCelsius()
+temp1.convertFahrenheit()
